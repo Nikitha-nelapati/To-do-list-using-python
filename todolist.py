3d83d39d74f01af7fa94b4949f34b960aea8a506
Nikitha-nelapati <163424951+Nikitha-nelapati@users.noreply.github.com>
2024-03-17 16:41:55 +0530

    Add files via upload

---
+++ todolist.py
@@ -0,0 +1,22 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/DELL/OneDrive/Documents/website/python project/todolist.py =
+
+= RESTART: C:\Users\DELL\OneDrive\Documents\website\python project\todolist.py =
+
+= RESTART: C:\Users\DELL\OneDrive\Documents\website\python project\todolist.py =
+Traceback (most recent call last):
+  File "C:\Users\DELL\OneDrive\Documents\website\python project\todolist.py", line 44, in <module>
+    button_frame = tk.Frame(window, bg="peach")
+  File "C:\Users\DELL\AppData\Local\Programs\Python\Python312\Lib\tkinter\__init__.py", line 3208, in __init__
+    Widget.__init__(self, master, 'frame', cnf, {}, extra)
+  File "C:\Users\DELL\AppData\Local\Programs\Python\Python312\Lib\tkinter\__init__.py", line 2648, in __init__
+    self.tk.call(
+_tkinter.TclError: unknown color name "peach"
+>>> 
+= RESTART: C:\Users\DELL\OneDrive\Documents\website\python project\todolist.py =
+
+= RESTART: C:\Users\DELL\OneDrive\Documents\website\python project\todolist.py =
+
+= RESTART: C:\Users\DELL\OneDrive\Documents\website\python project\todolist.py =
